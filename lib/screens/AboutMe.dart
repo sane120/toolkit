1c7c3519601d3835633d87157316b3279f868483
sane120 <sandro.merkt@gmail.com>
2021-05-25 02:02:57 +0200

    new colorscheme for My Business Card

--- lib/screens/AboutMe.dart
+++ lib/screens/AboutMe.dart
@@ -13,7 +13,7 @@ class _AboutMeState extends State<AboutMe> {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text('My Business Card'),
-          backgroundColor: Color.fromRGBO(	0, 150, 136, 1),
+          backgroundColor: Color(0xffb999900),
         ),
         body: Container(
           child: CardGo(),
@@ -40,7 +40,7 @@ class CardGo extends StatelessWidget {
                   style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 40.0,
-                    color: Colors.white,
+                    color: Color(0xffb999900),
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -48,7 +48,7 @@ class CardGo extends StatelessWidget {
                   'TRAINEE SOFTWARE DEVELOPER',
                   style: TextStyle(
                     fontFamily: 'Source Sans Pro',
-                    color: Colors.teal.shade100,
+                    color: Color(0xffb999900),
                     fontSize: 20.0,
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold,
@@ -58,67 +58,71 @@ class CardGo extends StatelessWidget {
                   height: 20.0,
                   width: 150.0,
                   child: Divider(
-                    color: Colors.teal.shade100,
+                    color: Color(0xffb999900),
                   ),
                 ),
                 Card(
+                    color: Color(0xffb666666),
                     margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                     child: ListTile(
                       leading: Icon(
                         Icons.phone,
-                        color: Colors.teal,
+                        color: Color(0xffb999900),
                       ),
                       title: Text(
                         '+41 44 452 11 40',
                         style: TextStyle(
-                          color: Colors.teal.shade900,
+                          color: Color(0xffb999900),
                           fontFamily: 'Source Sans Pro',
                           fontSize: 20.0,
                         ),
                       ),
                     )),
                 Card(
+                    color: Color(0xffb666666),
                     margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                     child: ListTile(
                       leading: Icon(
                         Icons.email,
-                        color: Colors.teal,
+                        color: Color(0xffb999900),
                       ),
                       title: Text(
                         'sandro.merkt@numas.ch',
                         style: TextStyle(
                             fontSize: 20.0,
-                            color: Colors.teal.shade900,
+                            color: Color(0xffb999900),
                             fontFamily: 'Source Sans Pro'),
                       ),
                     )),
                 Card(
+                    color: Color(0xffb666666),
                     margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                     child: ListTile(
                       leading: Icon(
                         Icons.home,
-                        color: Colors.teal,
+                        color: Color(0xffb999900),
                       ),
                       title: Text(
                         'Freigutstrasse 26, 8002 Zurich',
                         style: TextStyle(
                             fontSize: 20.0,
-                            color: Colors.teal.shade900,
+                            color: Color(0xffb999900),
                             fontFamily: 'Source Sans Pro'),
                       ),
                     )),
                 Card(
+                    color: Color(0xffb666666),
                     margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                     child: ListTile(
                       leading: Icon(
                         Icons.laptop_chromebook,
-                        color: Colors.teal,
+                        color: Color(0xffb999900),
                       ),
                       title: Text(
                         'www.numas.ch',
                         style: TextStyle(
                             fontSize: 20.0,
-                            color: Colors.teal.shade900,
+                            color: Color(0xffb999900),
                             fontFamily: 'Source Sans Pro'),
                       ),
                     )),
